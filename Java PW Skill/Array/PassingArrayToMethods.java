03ecdf4f2dca0d78496d01b823e9f0309d90d9a3
Heykrishna <heykrishna35@gmail.com>
2025-02-14 16:11:24 +0530

    pushing code

---
+++ Java PW Skill/Array/PassingArrayToMethods.java
@@ -0,0 +1,16 @@
+
+public class PassingArrayToMethods {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        System.out.println(x);
+
+        change(x);
+        System.out.println(change);
+    }
+
+    public static void change(int x) {
+        x = 10;
+    }
+}
