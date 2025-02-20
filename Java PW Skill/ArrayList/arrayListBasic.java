1e16e22941f72b6e4f81e6f4271ed96898d5d901
Heykrishna <heykrishna35@gmail.com>
2025-02-20 06:40:09 +0530

    Pushing code

---
+++ Java PW Skill/ArrayList/arrayListBasic.java
@@ -0,0 +1,29 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class arraylistBasic {
+    public static void main(String[] args) {
+        
+        // int[] arr = new int[6];
+        // double[] brr = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+        ArrayList<Integer> arr = new ArrayList<>(6);
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        arr.add(40);
+        arr.add(50);
+        arr.add(60);
+
+        System.out.println(arr);
+
+        
+
+
+
+
+        
+
+       
+    }
+}
