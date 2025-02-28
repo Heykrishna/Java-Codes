5591edb6decc64d474320c9711a57fb1e35e1ea0
Heykrishna <heykrishna35@gmail.com>
2025-02-28 06:49:07 +0530

    Pushing changes

---
+++ Java PW Skill/Binary Search/bs.java
@@ -0,0 +1,6 @@
+
+public class bs {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java PW Skill/LinkedList/ListNodeClass.java
@@ -0,0 +1,27 @@
+
+
+class Car {
+    int price;
+    String name;
+
+    // Car(int price, String name) {
+    //     // Constructor
+    //     this.price = price;
+    //     this.name = name;
+    // }
+
+
+
+    5
+}
+
+public class ListNodeClass {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.name = "Kia Sonet";
+        c1.price = 120000;
+
+        System.out.println("Car Name: " + c1.name);
+        System.out.println("Car Price: " + c1.price);
+    }
+}
