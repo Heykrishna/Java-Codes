0b80e524b5b986fb31dad15203dccb7599e04c1e
Heykrishna <heykrishna35@gmail.com>
2025-03-06 00:35:08 +0530

    Commiting changes

--- Java PW Skill/Recursion/Factorial.java
+++ Java PW Skill/Recursion/Factorial.java
@@ -2,8 +2,15 @@
 public class Factorial {
     public static void main(String[] args) {
         
+        int number = 5; // example input
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
 
-
-        
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
